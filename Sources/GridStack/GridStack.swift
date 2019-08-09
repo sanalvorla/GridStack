4e1026b28ba05a68f7dcfea2f1e4a20f7e2c05f1
Santiago Alvarado <salvaradoorlandini@gmail.com>
2019-08-09 12:55:17 -0500

    Updated for XCode Beta 5.

--- Sources/GridStack/GridStack.swift
+++ Sources/GridStack/GridStack.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 public struct GridStack<Content>: View where Content: View {
-    private let minCellWidth: Length
-    private let spacing: Length
+    private let minCellWidth: CGFloat
+    private let spacing: CGFloat
     private let numItems: Int
     private let alignment: HorizontalAlignment
     private let content: (Int, CGFloat) -> Content
     private let gridCalculator = GridCalculator()
     
     public init(
-        minCellWidth: Length = 100,
-        spacing: Length = 0,
+        minCellWidth: CGFloat = 100,
+        spacing: CGFloat = 0,
         numItems: Int = 0,
         alignment: HorizontalAlignment = .leading,
         @ViewBuilder content: @escaping (Int, CGFloat) -> Content
@@ -62,14 +62,14 @@ fileprivate extension Array {
 @available(iOS 13.0, OSX 10.15, tvOS 13.0, watchOS 6.0, *)
 private struct InnerGrid<Content>: View where Content: View {
     
-    private let spacing: Length
+    private let spacing: CGFloat
     private let rows: [[Int]]
     private let alignment: HorizontalAlignment
     private let content: (Int, CGFloat) -> Content
-    private let columnWidth: Length
+    private let columnWidth: CGFloat
     
     init(
-        spacing: Length,
+        spacing: CGFloat,
         items: [Int],
         alignment: HorizontalAlignment = .leading,
         @ViewBuilder content: @escaping (Int, CGFloat) -> Content,
@@ -87,7 +87,7 @@ private struct InnerGrid<Content>: View where Content: View {
             VStack(alignment: alignment, spacing: spacing) {
                 ForEach(rows, id: \.self) { row in
                     HStack(spacing: self.spacing) {
-                        ForEach(row) { item in
+                        ForEach(row, id: \.self) { item in
                             // Pass the index and the cell width to the content
                             self.content(item, self.columnWidth)
                                 .frame(width: self.columnWidth)
